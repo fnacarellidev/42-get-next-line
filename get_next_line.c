18daf47b9f38639ac7cdcdcb773cf29ea80f614b
fnacarel <fnacarel@student.42sp.org.br>
2022-09-21 16:56:16 +0200

    restarting gnl from scratch

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,26 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 20:30:28 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/16 00:10:11 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:55:27 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#define BUFFER_SIZE 42
 
 char	*get_next_line(int fd)
 {
-	static t_fdstruct	*fd_node;
-	t_fdstruct			*node_matching_fd;
-	char *c = "oi";
-
-	if (!fd_node)
-		fd_node = ft_newnode(fd);
-	else if (fd_node->fd == fd)
-		return (fd_node->line);
-	else if (has_matching_fd(fd_node, fd))
-	{
-		while (fd_node->fd != fd)
-			fd_node = fd_node->next;
-		printf("%d\n", fd_node->fd);
-		return (fd_node->line);
-	}
-	else
-		ft_nodeadd_back(&fd_node, ft_newnode(fd));
-	return (c);
+	static char	*str;
 }
 
 int main()
 {
 	int fd;
-	int	fd2;
-	char *c;
 
 	fd = open("testing.txt", O_RDONLY);
-	fd2 = open("testing2.txt", O_RDONLY);
-	printf("%d\n", fd);
-	c = get_next_line(fd);
-	printf("%d\n", fd2);
-	c = get_next_line(fd2);
-	c = get_next_line(fd2);
+
 	close(fd);
-	close(fd2);
+	printf("%s\n", get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:22:56 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/16 19:29:32 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:54:42 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,6 @@
 # define GNL_H
 # include <stdlib.h>
 
-typedef struct			s_fdstruct
-{
-	int					fd;
-	int					index_to_search;
-	char				*line;
-	struct s_fdstruct	*next;
-}						t_fdstruct;
-
 char		*get_next_line(int fd);
-int			has_matching_fd(t_fdstruct *node, int fd);
-void		ft_nodeadd_back(t_fdstruct **head, t_fdstruct *new);
-t_fdstruct	*ft_newnode(int fd);
-t_fdstruct	*ft_nodelast(t_fdstruct *node);
 
 #endif
